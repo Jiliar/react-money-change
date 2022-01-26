3f183ebd827d0c224918839769f135eb34824dac
Jiliar Silgado <jiliar_silgado@epam.com>
2022-01-25 20:20:41 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import Dinero from 'dinero.js';
+import RatesPromise from './rates';
+import { useEffect, useState } from 'react';
+
+
+Dinero.globalExchangeRatesApi = {
+  currency: 'EUR',
+  endpoint: RatesPromise
+}
+
+let CurrencySelector = ({rates, onChange}) =>{
+ return <select onChange={onChange}>{rates.map((rate)=><option key={rate}>{rate}</option>)}</select>
+}
 
 function App() {
+
+  let [rates, setRates] = useState([]);
+
+  let [originCurrency, setOriginCurrency] = useState([]);
+  let [targetCurrency, setTargetCurrency] = useState([]);
+  let [finalAmount, setFinalAmount] = useState([]);
+  let [originalAmount, SetOriginalAmount] = useState([]);
+
+  useEffect((rates)=>{
+    let ratesData;
+    RatesPromise.then((value)=>{
+      ratesData = value;
+      let ratesArray = Object.keys(ratesData.rates);
+      setRates(ratesArray)
+    })
+    //Init Components
+
+  }, []);
+
+  let convert = async () =>{
+    let amount = Dinero(
+      {amount: parseFloat(originalAmount) * 100,
+       currency: originCurrency
+      });
+
+      let result = await amount.convert(targetCurrency);
+
+      setFinalAmount(result.toFormat())
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <h1>Conversión de monedas</h1>
+        <div className='flex-container'>
+          <div>
+            <p>Origen: {originCurrency}</p>
+              <CurrencySelector 
+              onChange={(ev)=> setOriginCurrency(ev.target.value)}
+              rates={rates}/>
+          </div>
+          <div>
+            <p>Destino: {targetCurrency}</p>
+            <CurrencySelector 
+             onChange={(ev)=> setTargetCurrency(ev.target.value)}
+            rates={rates}/>
+          </div>
+        </div>
+
+        <input type="number" 
+        className='form-control' 
+        placeholder="Monto en centavos" 
+        onChange={(ev)=>SetOriginalAmount(ev.target.value)}/>
+        <p>El resultado es: {finalAmount}</p>
+
+        <button onClick={ convert} className='app-button'></button>
+      </div>
     </div>
   );
 }
